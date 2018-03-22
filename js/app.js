1efe9fa699ad2760a0d5615ba9096c2b1ddae3a1
Samuel James Thompson <noreply@gmail.com>
2018-03-22 20:20:54 +0200

    Add new deck JS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-/*
- * Create a list that holds all of your cards
- */
+//Variables needed
 
+const pack = document.querySelector('.pack');
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+let cardDeck = [
+    'fa-diamond', 'fa-diamond',
+    'fa-paper-plane-o', 'fa-paper-plane-o',
+    'fa-anchor', 'fa-anchor',
+    'fa-bolt', 'fa-bolt',
+    'fa-cube', 'fa-cube',
+    'fa-leaf', 'fa-leaf',
+    'fa-bicycle', 'fa-bicycle',
+    'fa-cheese', 'fa-cheese'
+]
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -24,6 +27,38 @@ function shuffle(array) {
 
     return array;
 }
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+function newDeck() {
+    //Shuffle
+    cardDeck = shuffle(cardDeck);
+    //Build HTML for deck
+    const fragment = document.createDocumentFragment();
+    const deck = document.createElement('ul');
+    deck.classList.add('deck');
+    fragment.appendChild(deck);
+    cardDeck.forEach(function(cardIcon) {
+        const cardHolder = document.createElement('li');
+        cardHolder.classList.add('card');
+        const cardFace = document.createElement('i');
+        cardFace.classList.add('fa', cardIcon);
+        cardHolder.appendChild(cardFace);
+        deck.appendChild(cardHolder);
+    })
+    //Remove existing Deck
+    while (pack.firstChild) {
+      pack.removeChild(pack.firstChild);
+    }
+    //Add HTML to DOM
+    pack.appendChild(fragment);
+}
+
+
 
 
 /*
