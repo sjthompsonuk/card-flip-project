0c6c9bfdf8e1afa9547ffddea8860630fdff8020
Samuel James Thompson <noreply@gmail.com>
2018-03-22 23:56:56 +0200

    Add card flipping logic and function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 //Variables needed
-
+const reshuffle = document.querySelector('.fa-repeat');
 const pack = document.querySelector('.pack');
+let moves = 0;
+const moveCounter = document.querySelector('.moves');
+let matches = 0;
 
 let cardDeck = [
     'fa-diamond', 'fa-diamond',
@@ -10,8 +13,8 @@ let cardDeck = [
     'fa-cube', 'fa-cube',
     'fa-leaf', 'fa-leaf',
     'fa-bicycle', 'fa-bicycle',
-    'fa-cheese', 'fa-cheese'
-]
+    'fa-bomb', 'fa-bomb'
+];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -34,6 +37,8 @@ function shuffle(array) {
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+// New Deck function
 function newDeck() {
     //Shuffle
     cardDeck = shuffle(cardDeck);
@@ -46,7 +51,8 @@ function newDeck() {
         const cardHolder = document.createElement('li');
         cardHolder.classList.add('card');
         const cardFace = document.createElement('i');
-        cardFace.classList.add('fa', cardIcon);
+        cardFace.classList.add('fa');
+        cardFace.classList.add(cardIcon);
         cardHolder.appendChild(cardFace);
         deck.appendChild(cardHolder);
     })
@@ -56,9 +62,16 @@ function newDeck() {
     }
     //Add HTML to DOM
     pack.appendChild(fragment);
+    //Reset move counter
+    moves = 0;
+    moveCounter.textContent = moves.toString();
 }
 
+//Event Listener for newDeck
 
+reshuffle.addEventListener('mousedown', function() {
+    newDeck();
+});
 
 
 /*
@@ -71,3 +84,16 @@ function newDeck() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ // Card listener
+
+pack.addEventListener('click', function(evt) {
+    if ((evt.target.nodeName == 'LI') && (evt.target.classList.contains('open') == false) && (evt.target.classList.contains('match') == false)) {
+        moves += 1;
+        moveCounter.textContent = moves.toString();
+        //display card
+        evt.target.classList.add('open');
+        evt.target.classList.add('show');
+        //addToOpenList(evt.target);
+    }
+})
